34732619ac991ce638d7ea776f286f9b2f927d44
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-03 04:13:56 +0530

    Basic framework for displaying with templates

--- journal/journal.py
+++ journal/journal.py
@@ -1,36 +1,43 @@
 import os
 import glob
 from datetime import datetime
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, render_template
+import requests
 
 
 
 app = Flask(__name__)
 
 def get_entries(name):
-	home = os.path.expanduser('~')
-	journaldir = os.path.join(home, '.journalist', name)
-	if not os.path.exists(journaldir):
-		return False
+    home = os.path.expanduser('~')
+    journaldir = os.path.join(home, '.journalist', name)
+    if not os.path.exists(journaldir):
+        return False
 
-	entries = []
-	for file in glob.glob(os.path.join(journaldir, '**', '*.md'), recursive=True):
-		filename =  os.path.basename(file)
-		dt = datetime.strptime(filename, '%Y-%m-%d-%a.md')
-		with open(file) as f:
-			contents = f.read()
-		entries.append((dt, contents))
+    entries = []
+    for file in glob.glob(os.path.join(journaldir, '**', '*.md'), recursive=True):
+        filename =  os.path.basename(file)
+        dt = datetime.strptime(filename, '%Y-%m-%d-%a.md')
+        with open(file) as f:
+            contents = f.read()
+        entries.append((dt, contents))
 
-	entries.sort(key=lambda x: x[0])
-	return entries
+    entries.sort(key=lambda x: x[0])
+    return entries
+
+
+def githubify(text):
+    r = requests.post('https://api.github.com/markdown/raw', data=text, headers={'Content-Type': 'text/x-markdown'})
+    return r.text
 
 
 @app.route('/journalist')
 def view_journal():
-	name = request.args.get('name')
-	entries = get_entries(name)
+    name = request.args.get('name')
+    entries = get_entries(name)
 
-	text = ''
-	for entry in entries:
-		text = text + entry[1] + '<br><br>'
-	return text
+    text = ''
+    for entry in entries:
+        text = text + entry[1] + '<br><br>'
+    text = '__Sounds__ good? I _think_ [so](https://www.google.com).'
+    return render_template('journal.html', article=githubify(text))
--- journalist.py
+++ journalist.py
@@ -8,43 +8,43 @@ import subprocess
 
 
 def response(question):
-	answer = input('[?] {}: [y/n] '.format(question))
-	if not answer or answer[0].lower() != 'y':
-	    return False
-	return True
+    answer = input('[?] {}: [y/n] '.format(question))
+    if not answer or answer[0].lower() != 'y':
+        return False
+    return True
 
 
 def main():
-	with open('config.yaml') as f:
-		config = yaml.load(f)
+    with open('config.yaml') as f:
+        config = yaml.load(f)
 
-	home = os.path.expanduser('~')
-	datadir = os.path.join(home, '.journalist')
+    home = os.path.expanduser('~')
+    datadir = os.path.join(home, '.journalist')
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('name', help='The name of the journal', type=str)
-	args = parser.parse_args()
-	name = args.name
+    parser = argparse.ArgumentParser()
+    parser.add_argument('name', help='The name of the journal', type=str)
+    args = parser.parse_args()
+    name = args.name
 
-	journal_path = os.path.join(datadir, name)
-	if not os.path.exists(journal_path):
-		print('[!] The journal {} does not exist!'.format(name))
-		res	= response('Do you want to create journal {}'.format(name))
-		if res:
-			os.makedirs(journal_path)
-		else:
-			sys.exit()
+    journal_path = os.path.join(datadir, name)
+    if not os.path.exists(journal_path):
+        print('[!] The journal {} does not exist!'.format(name))
+        res = response('Do you want to create journal {}'.format(name))
+        if res:
+            os.makedirs(journal_path)
+        else:
+            sys.exit()
 
-	date = datetime.now()
-	filename = date.strftime('%Y-%m-%d-%a') + '.md'
-	curdir = os.path.join(datadir, name, date.strftime('%Y'), date.strftime('%m'))
-	if not os.path.exists(curdir):
-		os.makedirs(curdir)
+    date = datetime.now()
+    filename = date.strftime('%Y-%m-%d-%a') + '.md'
+    curdir = os.path.join(datadir, name, date.strftime('%Y'), date.strftime('%m'))
+    if not os.path.exists(curdir):
+        os.makedirs(curdir)
 
-	filepath = os.path.join(curdir, filename)
-	subprocess.call([config['Global']['Editor'], filepath])
+    filepath = os.path.join(curdir, filename)
+    subprocess.call([config['Global']['Editor'], filepath])
 
 
 
 if __name__=='__main__':
-	main()
+    main()
