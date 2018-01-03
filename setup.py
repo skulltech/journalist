d7df66a8964baafd2fb1a09343c5e87dfe9afd96
Sumit Ghosh <sumit.ghosh32@gmail.com>
2018-01-03 20:42:33 +0530

    Update repo URL. Fixes #1

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/SkullTech/journal',  # Optional
+    url='https://github.com/SkullTech/journalist',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
